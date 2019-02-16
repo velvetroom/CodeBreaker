d17c7ac94f14f2877a8d21e8e76b54888b03d480
vauxhall <zero.griffin@gmail.com>
2019-02-16 16:07:00 +0100

    Project setup

--- Mac/App.swift
+++ Mac/App.swift
@@ -1,5 +1,16 @@
 import AppKit
 
 @NSApplicationMain class App: NSWindow, NSApplicationDelegate {
+    static private(set) weak var shared: App!
     
+    func applicationShouldTerminateAfterLastWindowClosed(_: NSApplication) -> Bool { return true }
+    override func cancelOperation(_: Any?) { makeFirstResponder(nil) }
+    override func mouseDown(with: NSEvent) { makeFirstResponder(nil) }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = .black
+        NSApp.delegate = self
+        App.shared = self
+    }
 }
