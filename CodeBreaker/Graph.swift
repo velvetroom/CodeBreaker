eb34d08258c22f71dab0e3746b8be782e6499904
vauxhall <zero.griffin@gmail.com>
2019-02-20 09:09:33 +0100

    Implement graph

--- CodeBreaker/Graph.swift
+++ CodeBreaker/Graph.swift
@@ -2,11 +2,17 @@ class Graph {
     private(set) var route = [Vertex]()
     
     init(_ message: String, level: Level) {
-        message.map({ String($0) }).forEach { route.append ({
+        message.map({ String($0) }).enumerated().forEach { route.append ({
             route.isEmpty ? Vertex($1) : {
                 $0.node = $1
                 return $0
             } (route.last!.node!.vertex.first(where: { $0.node == nil })!, $1)
-        } ($0, Node($0, size: level.lanes.count > 1 ? 2 : 1)))}
+        } ($0.1, { c in
+            route.first(where: { $0.node?.state == c.1 &&
+                (c.0 == message.count - 1 ||
+                    { ($1 > 0 && $0 !== route.last) || $1 > 1 } ($0, $0.node!.vertex.filter({ $0.node == nil }).count)
+                ) })?.node ??
+                Node(c.1, size: level.lanes.count > 1 ? 2 : 1)
+        } ($0) ))}
     }
 }
--- Tests/TestGraph.swift
+++ Tests/TestGraph.swift
@@ -52,4 +52,53 @@ class TestGraph: XCTestCase {
         XCTAssertTrue(graph.route.first?.node === graph.route.last?.node)
         XCTAssertEqual(2, graph.route.count)
     }
+    
+    func testABB() {
+        let graph = Graph("abb", level: Rookie())
+        XCTAssertEqual("a", graph.route.first?.node?.state)
+        XCTAssertEqual("b", graph.route[1].node?.state)
+        XCTAssertEqual("b", graph.route[2].node?.state)
+        XCTAssertTrue(graph.route[1].node === graph.route[2].node)
+        XCTAssertEqual(3, graph.route.count)
+    }
+    
+    func testAABRookie() {
+        let graph = Graph("aab", level: Rookie())
+        XCTAssertEqual("a", graph.route[0].node?.state)
+        XCTAssertEqual("a", graph.route[1].node?.state)
+        XCTAssertEqual("b", graph.route[2].node?.state)
+        XCTAssertTrue(graph.route[0].node !== graph.route[1].node)
+        XCTAssertEqual(3, graph.route.count)
+    }
+    
+    func testAABAdvanced() {
+        let graph = Graph("aab", level: Advanced())
+        XCTAssertEqual("a", graph.route[0].node?.state)
+        XCTAssertEqual("a", graph.route[1].node?.state)
+        XCTAssertEqual("b", graph.route[2].node?.state)
+        XCTAssertTrue(graph.route[0].node === graph.route[1].node)
+        XCTAssertEqual(3, graph.route.count)
+    }
+    
+    func testAABBRookie() {
+        let graph = Graph("aabb", level: Rookie())
+        XCTAssertEqual("a", graph.route[0].node?.state)
+        XCTAssertEqual("a", graph.route[1].node?.state)
+        XCTAssertEqual("b", graph.route[2].node?.state)
+        XCTAssertEqual("b", graph.route[3].node?.state)
+        XCTAssertTrue(graph.route[0].node !== graph.route[1].node)
+        XCTAssertTrue(graph.route[2].node === graph.route[3].node)
+        XCTAssertEqual(4, graph.route.count)
+    }
+    
+    func testAABBAdvanced() {
+        let graph = Graph("aabb", level: Advanced())
+        XCTAssertEqual("a", graph.route[0].node?.state)
+        XCTAssertEqual("a", graph.route[1].node?.state)
+        XCTAssertEqual("b", graph.route[2].node?.state)
+        XCTAssertEqual("b", graph.route[3].node?.state)
+        XCTAssertTrue(graph.route[0].node === graph.route[1].node)
+        XCTAssertTrue(graph.route[2].node === graph.route[3].node)
+        XCTAssertEqual(4, graph.route.count)
+    }
 }
